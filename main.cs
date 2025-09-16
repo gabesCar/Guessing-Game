a454259be79549e37ecc50a53031ddc87c733e88
Gabs <cardosogabrielle601@gmail.com>
2025-09-15 22:31:11 -0300

    Delete main.cs

--- main.cs
+++
@@ -1,9 +0,0 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        int resultado = Calculadora.Somar(5, 3);
-        Console.WriteLine("Resultado: " + resultado);
-    }
-}
-// testando formas de chamar um cs para o main.cs
