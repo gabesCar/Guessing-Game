bd7ee292491001568816396051d92159cb891c20
Gabs <cardosogabrielle601@gmail.com>
2025-09-15 22:30:39 -0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿for(int contador = 5; contador <= 20; contador++)
-{
-    Console.WriteLine("Valor do contador é " + contador);
-}
-Console.WriteLine("fim");
-
-
-//testes de códigos utilizando for 
